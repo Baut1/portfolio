dee4c09e7cd9c16de09f0dad2eb3945d19a94c3d
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2023-12-13 21:40:48 -0300

    dates properly showing, darkmode implemented, various fixes

--- src/components/contributions.vue
+++ src/components/contributions.vue
@@ -1,24 +1,41 @@
 <script setup lang="ts">
-let arrContributions = [{ date: '2023-9-22', count: 6 }]
+const { t } = useI18n()
+const contributions = ref([])
 
+let arrContributions: any = [{ date: '2023-9-22', count: 6 }]
+
+const darkRange = ['#1f1f22', '#333333', '#1d466c', '#1d5689', '#1d69ac', '#1B95D1']
+const lightRange = ['#ebedf0', '#eeeeee', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e']
+let colorRange = darkRange
+
+// api call for contributions list
 const getContributions = async () => {
   await fetch('https://github-contributions-api.jogruber.de/v4/baut1')
     .then(res => res.json())
     .then(response => arrContributions = response.contributions)
+  contributions.value = arrContributions
 }
-
 getContributions()
 
-alert(arrContributions)
+// if(isDark.value === true) {
+//     colorRange = darkRange;
+// } else {
+//     colorRange = lightRange;
+// }
+
+isDark.value ? colorRange = darkRange : colorRange = lightRange
 </script>
 
 <template>
-  <div flex flex-col mx-12>
-    <h1>contribs</h1>
-    <calendar-heatmap :end-date="2018 - 9 - 22" :values="[{ date: '1969-6-6', count: 6 }]" dark-mode />
+  <div flex flex-col>
+    <h2 text-5xl font-700>
+      {{ t('contributions.title') }}
+    </h2>
+    <span flex justify-center mt-6>
+      <calendar-heatmap :end-date="['2023-12-13']" :values="contributions" :range-color="colorRange" w-4xl />
+    </span>
   </div>
 </template>
 
 <style>
-
 </style>
